d27fbd33ff55e162f41e3701b28725f2c61880d0
fitraifron-maker <fitraifron@gmail.com>
2025-12-07 18:20:42 +0700

    Create NamaMahasiswa.java

---
+++ PBOSem1/Pertemuan8(UTS)/NamaMahasiswa.java
@@ -0,0 +1,11 @@
+public class NamaMahasiswa{
+    public static void main(String[] args) {
+        Mahasiswa m = new Mahasiswa("Fitra", 18);
+
+        //m.sayHello();
+        m.setUmur(-5);
+        m.setUmur(20);
+        m.sayHello();
+    }
+
+}
